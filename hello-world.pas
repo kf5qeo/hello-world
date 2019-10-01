e8550ba67b9dfd7367f33c504b42e98cebea9f0c
John H. Guillory <westlakegeek@yahoo.com>
2019-10-01 08:14:12 -0500

    added source code
    Added source code in Virtual Pascal and binary files

---
+++ hello-world.pas
@@ -0,0 +1,14 @@
+program helloworld;
+uses crt, dos,use32;
+var mm,dd,yy,hh,mi,ss,hos,dow: word;
+
+begin
+  textbackground(black);
+  textcolor(lightCyan);
+  clrscr;
+  getDate(yy,mm,dd,dow);
+  gettime(hh,mi,ss,hos);
+  Writeln('Hello World!');
+  Writeln('Today is ',mm,'/',dd,'/',yy);
+  Writeln('The time is ',hh,':',mm,':',ss,'.',hos);
+end.
